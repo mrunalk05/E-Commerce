8c88fe831133351cdb9393513fe39dfa9dec7ef9
Sharvari <sharvariypatil@gmail.com>
2022-11-25 13:38:27 +0530

    Add to cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -13,7 +13,35 @@ for(var i=0;i<updateBtns.length;i++)
         }
         else
         {
-            console.log('User is logged in,Sending data...')
+            updateUserOrder(productId,action)
         }
     })
+}
+
+
+
+function updateUserOrder(productId,action)
+{
+    console.log('User is logged in,Sending data...')
+
+    var url='/update_item/'
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify( {'productId':productId,'action':action})
+       
+    })
+
+    .then((response)=>{
+        return response.json()
+    })
+
+    .then((data)=>{
+        console.log('data:',data)
+    })
+
+   
 }
